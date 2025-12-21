9d7fb844d3ad71ae20bfe63813daf5e4bb7aa9e3
Remars78 <132569466+Remars78@users.noreply.github.com>
2025-12-21 22:36:59 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,25 +14,44 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    
+
     buildFeatures {
         compose = true
     }
+    
+    // Важная настройка для Java 17
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_10 // Используем синтаксис 1.8+
+        targetCompatibility = JavaVersion.VERSION_1_10
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        // Эта версия жестко привязана к Kotlin 1.9.22
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
-    implementation(composeBom)
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    // Основные библиотеки Compose (версии прописаны явно)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     
-    // Сеть и Картинки
+    // UI
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.compose.ui:ui-graphics:1.6.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    
+    // Network & Image
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    
+    // Icons
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
 }
+
